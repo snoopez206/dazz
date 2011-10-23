6ab57cbda369727c7f89be8ec2fb2b93d0bf1bfc
chatree.srichart@gmail.com <chatree.srichart@gmail.com@8de61735-b931-d40d-4ba5-0e3e53333d46>
2011-10-23 14:30:12 +0000

    implement testSentDetect and testSentPosDetect test cases for SentenceDetector

--- trunk/src/com/dazz/opennlp/test/SentenceDetectorTests.java
+++ trunk/src/com/dazz/opennlp/test/SentenceDetectorTests.java
@@ -3,6 +3,16 @@
  */
 package com.dazz.opennlp.test;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStream;
+
+import opennlp.tools.sentdetect.SentenceDetectorME;
+import opennlp.tools.sentdetect.SentenceModel;
+import opennlp.tools.util.Span;
+
+import org.ofbiz.base.util.Debug;
+import org.ofbiz.base.util.FileUtil;
 import org.ofbiz.service.testtools.OFBizTestCase;
 
 public class SentenceDetectorTests extends OFBizTestCase {
@@ -13,4 +23,29 @@ public class SentenceDetectorTests extends OFBizTestCase {
         super(name);
     }
 
+    public void testSentDetect() throws Exception {
+        File modelFile = FileUtil.getFile("component://dazz/models/en-sent.bin");
+        InputStream in = new FileInputStream(modelFile);
+        SentenceModel model = new SentenceModel(in);
+        SentenceDetectorME detector = new SentenceDetectorME(model);
+        String rawText = "  First sentence. Second sentence. ";
+        Debug.logInfo("raw text: " + "\"" + rawText + "\"", module);
+        String[] sentences = detector.sentDetect(rawText);
+        for (String sentence : sentences) {
+            Debug.logInfo("sentence: \"" + sentence + "\"", module);
+        }
+    }
+    
+    public void testSentPosDetect() throws Exception {
+        File modelFile = FileUtil.getFile("component://dazz/models/en-sent.bin");
+        InputStream in = new FileInputStream(modelFile);
+        SentenceModel model = new SentenceModel(in);
+        SentenceDetectorME detector = new SentenceDetectorME(model);
+        String rawText = "  First sentence. Second sentence. ";
+        Debug.logInfo("raw text: " + "\"" + rawText + "\"", module);
+        Span[] sentences = detector.sentPosDetect(rawText);
+        for (Span sentence : sentences) {
+            Debug.logInfo("sentence: \"" + sentence + "\"", module);
+        }
+    }
 }
