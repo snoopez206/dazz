83a6135af2520bf706f571a6515c12bc146662be
chatree.srichart <chatree.srichart@gmail.com>
2012-04-14 06:58:57 +0000

    Initail Natural Language and Knowledge Base screens

--- src/com/dazz/opennlp/TokenizerServices.java
+++
@@ -1,9 +0,0 @@
-/**
- * Licensed to the dazz brainy artificial intelligence
- */
-package com.dazz.opennlp;
-
-public class TokenizerServices {
-
-    public final static String module = TokenizerServices.class.getName();
-}
