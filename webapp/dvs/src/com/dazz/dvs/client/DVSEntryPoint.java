dffd08804d9186fe69448c33684d4fee30c03114
chatree.srichart <chatree.srichart@gmail.com>
2012-05-24 00:15:45 +0000

    - create dvs webapp

---
+++ webapp/dvs/src/com/dazz/dvs/client/DVSEntryPoint.java
@@ -0,0 +1,13 @@
+package com.dazz.dvs.client;
+
+import com.google.gwt.core.client.EntryPoint;
+import com.google.gwt.user.client.Window;
+
+public class DVSEntryPoint implements EntryPoint {
+
+	@Override
+	public void onModuleLoad() {
+		Window.alert("DVS");
+	}
+
+}
